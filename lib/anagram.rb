d60295aed9a43e64eac1c34cde274c2a01dda11a
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-09 01:49:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram 
+  
+  attr_accessor :words
+  
+  def initialize(words)
+    @words = words
+  end
+  
+ def match(array)
+    array.select do |words|
+      words.split("").sort == @words.split("").
+   end 
+  end
+ end 
